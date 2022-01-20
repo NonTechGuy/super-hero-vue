0268885ed630a66b90638291c49232126013aefa
Craig Bartlett <craig.bartlett@syw.io>
2022-01-20 06:55:10 +0200

    task: (Basic requirements) add and configure axios

--- src/views/SuperheroView.vue
+++ src/views/SuperheroView.vue
@@ -1,11 +1,27 @@
 <template>
   <div>
     Super Hero
+    <button @click="fetchAPI">fetch</button>
   </div>
 </template>
 
 <script lang="ts">
-export default {};
+import axios from 'axios';
+
+export default {
+  data() {},
+  mounted() {
+    axios
+      .get('https://akabab.github.io/superhero-api/api/all.json')
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  },
+  methods: {},
+};
 </script>
 
 <style lang="scss" scoped>
